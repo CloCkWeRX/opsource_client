94cb42328fa10a188bfab905b32ed759d7259239
Daniel O'Connor <daniel.o'connor@htw.com.au>
2013-08-14 12:11:59 +0930

    Add vcr

---
+++ spec/support/vcr.rb
@@ -0,0 +1,13 @@
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.allow_http_connections_when_no_cassette = true
+  c.hook_into :webmock
+  c.default_cassette_options = { record: :new_episodes}
+  c.configure_rspec_metadata!
+end
+
+RSpec.configure do |c|
+  c.treat_symbols_as_metadata_keys_with_true_values = true
+end
